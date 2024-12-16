5909ebc4854299520d8e64718c9d1d6a1fc54716
Maria <165724856+Matilda667@users.noreply.github.com>
2024-12-16 15:11:20 +0100

    Task-6

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -7,41 +7,41 @@
 const score = 70 // тестовое значение, можно изменять
 let grade
 // your code
-if (score >= 90 && score <= 100) {
-    grade = 'A';
-} else if (score >= 80 && score < 90) {
-    grade = 'B';
-} else if (score >= 70 && score < 80) {
-    grade = 'C';
-} else if (score >= 50 && score < 70) {
-    grade = 'D';
-} else if (score >= 0 && score < 50) {
-    grade = 'F';
-} else {
-    grade = 'Неверный балл'; // Если балл меньше 0 или больше 100
-}
-
-console.log(grade);
-
-
-// switch (true) {
-//     case (score >= 90 && score <= 100):
-//         grade = 'A';
-//         break;
-//     case (score >= 80 && score < 90):
-//         grade = 'B';
-//         break;
-//     case (score >= 70 && score < 80):
-//         grade = 'C';
-//         break;
-//     case (score >= 50 && score < 70):
-//         grade = 'D';
-//         break;
-//     case (score >= 0 && score < 50):
-//         grade = 'F';
-//         break;
-//     default:
-//         grade = 'Неверный балл'; // Если балл меньше 0 или больше 100
+// if (score >= 90 && score <= 100) {
+//     grade = 'A';
+// } else if (score >= 80 && score < 90) {
+//     grade = 'B';
+// } else if (score >= 70 && score < 80) {
+//     grade = 'C';
+// } else if (score >= 50 && score < 70) {
+//     grade = 'D';
+// } else if (score >= 0 && score < 50) {
+//     grade = 'F';
+// } else {
+//     grade = 'Неверный балл'; // Если балл меньше 0 или больше 100
 // }
+
+// console.log(grade);
+
+
+switch (true) {
+    case (score >= 90 && score <= 100):
+        grade = 'A';
+        break;
+    case (score >= 80 && score < 90):
+        grade = 'B';
+        break;
+    case (score >= 70 && score < 80):
+        grade = 'C';
+        break;
+    case (score >= 50 && score < 70):
+        grade = 'D';
+        break;
+    case (score >= 0 && score < 50):
+        grade = 'F';
+        break;
+    default:
+        grade = 'Неверный балл'; // Если балл меньше 0 или больше 100
+}
 
 // console.log(grade);
