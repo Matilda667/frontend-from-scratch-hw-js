1ffe32385069e8b494514d819e6a948f08777a42
Maria <165724856+Matilda667@users.noreply.github.com>
2024-12-16 16:43:53 +0100

    Task-9

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -28,16 +28,16 @@ switch (true) {
     case (score >= 90 && score <= 100):
         grade = 'A';
         break;
-    case (score >= 80 && score < 89):
+    case (score >= 80 && score < 90):
         grade = 'B';
         break;
-    case (score >= 70 && score < 79):
+    case (score >= 70 && score < 80):
         grade = 'C';
         break;
-    case (score >= 50 && score <= 69):
+    case (score >= 50 && score < 70):
         grade = 'D';
         break;
-    case (score >= 0 && score <= 49):
+    case (score >= 0 && score < 50):
         grade = 'F';
         break;
     default:
